24a75cbb39d00537f8fde67b260d842e2884167f
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-17 23:02:46 +0530

    unit2prog2

--- unit2prog2.c
+++ unit2prog2.c
@@ -26,26 +26,3 @@ int main()
     return 0;
 }
 
-#include <stdio.h>
-
-int factorial(int n)
-{
-    if (n == 0 || n == 1)
-        return 1;
-    else
-        return n * factorial(n - 1);
-}
-
-int main()
-{
-    int n, result;
-
-    printf("Enter a number: ");
-    scanf("%d", &n);
-
-    result = factorial(n);
-
-    printf("Factorial of %d = %d", n, result);
-
-    return 0;
-}
